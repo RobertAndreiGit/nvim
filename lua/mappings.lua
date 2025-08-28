f8929f1dc1539e802998c6475f70755a58a42383
Robert Amarandei <robert_amarandei@yahoo.com>
2025-08-28 16:14:06 +0300

    Update configs

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -2,6 +2,27 @@ require "nvchad.mappings"
 
 -- add yours here
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+vim.keymap.set("n", "<leader>fk", ":Telescope keymaps<CR>", { desc = "Find keymaps" })
+
+vim.keymap.set("n", "<leader>tt", function()
+  require("neotest").run.run()
+end, { desc = "Run nearest test" })
+
+vim.keymap.set("n", "<leader>tf", function()
+  require("neotest").run.run(vim.fn.expand "%")
+end, { desc = "Run file tests" })
+
+vim.keymap.set("n", "<leader>ts", function()
+  require("neotest").summary.toggle()
+end, { desc = "Toggle test summary" })
+
+vim.keymap.set("n", "<leader>to", function()
+  require("neotest").output.open { enter = true }
+end, { desc = "Show test output" })
+
+vim.keymap.set("n", "<leader>ta", function()
+  require("neotest").playwright.attachment()
+end, { desc = "Launch Playwright test attachment" })
 
 local map = vim.keymap.set
 
@@ -9,3 +30,4 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -57,4 +57,65 @@ return {
     },
   },
   -- ~/.config/nvim/lua/plugins/init.lua
+  {
+    "andymass/vim-matchup",
+    init = function()
+      -- modify your configuration vars here
+      vim.g.matchup_treesitter_stopline = 500
+
+      -- or call the setup function provided as a helper. It defines the
+      -- configuration vars for you
+      require("match-up").setup {
+        treesitter = {
+          stopline = 500,
+        },
+      }
+    end,
+    -- or use the `opts` mechanism built into `lazy.nvim`. It calls
+    -- `require('match-up').setup` under the hood
+    -- -@type matchup.Config
+    opts = {
+      treesitter = {
+        stopline = 500,
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "thenbe/neotest-playwright",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("neotest").setup {
+        adapters = {
+          require("neotest-playwright").adapter {
+            options = {
+              persist_project_selection = true,
+              enable_dynamic_test_discovery = true,
+            },
+          },
+        },
+        consumers = {
+          playwright = require("neotest-playwright.consumers").consumers,
+        },
+      }
+    end,
+  },
+  {
+    "rmagatti/auto-session",
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    -- -@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+      -- log_level = 'debug',
+    },
+  },
 }
