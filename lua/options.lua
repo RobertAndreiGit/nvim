1c1141d5226bd3bc8a15a0634b8ac75ccda3abfe
Robert <robert_amarandei@yahoo.com>
2025-09-07 18:24:54 +0300

    Updates - no fold on open

--- lua/options.lua
+++ lua/options.lua
@@ -3,7 +3,12 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.opt
-o.cursorlineopt = 'both' -- to enable cursorline!
+o.cursorlineopt = "both" -- to enable cursorline!
 o.relativenumber = true
 o.scrolloff = 10
 o.termguicolors = true
+
+vim.o.foldcolumn = "1"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
